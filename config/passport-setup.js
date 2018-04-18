59bd731200382a59573d39555d2b0fb77b1dacff
frankwichw <frankwichw@gmail.com>
2018-04-18 18:02:52 -0400

    errant yarn.lock removed

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,2 +1,13 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20");
+var db = require("../models");
+
+passport.use(
+    new GoogleStrategy({
+    // strategy options
+
+    }),
+    (accessToken, refreshToken, profile, done) => {
+    // passport callback function
+    }
+)
