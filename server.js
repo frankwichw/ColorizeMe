dfe5bd6157d53d5430b7bbc28f17fd97e480cfaa
frankwichw <frankwichw@gmail.com>
2018-04-19 18:28:55 -0400

    static serving

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const PORT = process.env.PORT || 3001;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Serve up static assets
+app.use(express.static("client/build"));
+
 // Add routes, both API and view
 app.use(routes);
 
